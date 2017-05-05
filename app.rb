72b893671b90d8614e64133378cef7e649d96a6a
Spencer Alan <spencer.alan@icloud.com>
2017-05-05 14:36:26 -0700

    add front end styling for adding a volunteer and project

--- app.rb
+++ app.rb
@@ -1,15 +1,46 @@
-require 'sinatra'
-require 'sinatra/reloader'
-require './lib/volunteer'
-require './lib/project'
-require './lib/organization'
-require 'pry'
+require "sinatra"
+require "sinatra/reloader"
+require "./lib/volunteer"
+require "./lib/project"
+require "./lib/organization"
+require "pry"
+require "pg"
 
-also_reload('lib/**/*.rb')
+also_reload "lib/**/*.rb"
 
 DB = PG.connect({:dbname => "volunteer_tracker"})
 
 
-get('/') do
-  erb(:index)
+get "/" do
+  @volunteers = Organization.volunteers
+  @projects = Organization.projects
+  erb :index
+end
+
+post "/project" do
+  name = params.fetch("name")
+  project = Project.new({name: name})
+  project.save
+
+  @volunteers = Organization.volunteers
+  @projects = Organization.projects
+  erb :index
+end
+
+get "/project/:id" do
+  erb :project
+end
+
+post "/volunteer" do
+  name = params.fetch("name")
+  volunteer = Volunteer.new({name: name})
+  volunteer.save
+
+  @volunteers = Organization.volunteers
+  @projects = Organization.projects
+  erb :index
+end
+
+get "/volunteer/:id" do
+  erb :volunteer
 end
--- lib/organization.rb
+++ lib/organization.rb
@@ -2,11 +2,11 @@ require "securerandom"
 
 module Organization
   def self.volunteers
-    Volunteer.objectify(DB.exec("select * from volunteers"))
+    Volunteer.objectify(DB.exec("select * from volunteers order by name"))
   end
   
   def self.projects
-    Project.objectify(DB.exec("select * from projects"))
+    Project.objectify(DB.exec("select * from projects order by name"))
   end
 
   def self.assign_project_to_volunteer(volunteer_id, project_id)
